c7b309a9fe9639843e6087579fb2060637b28a0b
akhilesthenos <akhil@esthenos.com>
2022-03-08 18:27:38 +0530

    minor changes

--- src/components/brand/index.jsx
+++ src/components/brand/index.jsx
@@ -3,7 +3,11 @@ import PropTypes from "prop-types";
 const Brand = ({ data }) => {
     return (
         <div className="single-brand">
-            <img src={process.env.PUBLIC_URL + data.image} alt="brand logo" />
+            <img
+                src={process.env.PUBLIC_URL + data.image}
+                className="hover-effect"
+                alt="brand logo"
+            />
         </div>
     );
 };
